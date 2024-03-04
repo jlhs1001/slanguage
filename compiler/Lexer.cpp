4007ae19a2201b107d482ae5e08f85d8e67b857c
Liam <jlhs1001@gmail.com>
2024-03-04 14:48:47 -0500

    add file reading logic

--- compiler/Lexer.cpp
+++ compiler/Lexer.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "Lexer.h"
+
+std::string Lexer::readFile(const std::string &path) {
+    // Open the file in binary mode and position the file pointer at the end to obtain its size.
+    std::ifstream file(path, std::ios::binary | std::ios::ate);
+    if (!file) {
+        // If the file could not be opened, throw an exception with the path to aid in debugging.
+        throw std::runtime_error("Could not open file at " + path);
+    }
+
+    // Initialize a string with two iterators, reading the entire file contents into the string.
+    // The first iterator is the beginning of the file, and the second represents the EOF marker.
+    return {
+            (std::istreambuf_iterator<char>(file)),
+            (std::istreambuf_iterator<char>())
+    };
+}
+
+Lexer::Lexer(const std::string& sourcePath) {
+    // Load the source code from the file into a string.
+    source = readFile(sourcePath);
+}
+
+Lexer::~Lexer() = default;
--- compiler/Lexer.h
+++ compiler/Lexer.h
@@ -6,11 +6,37 @@
 #define SLANG_LEXER_H
 #include <string>
 #include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
 
 
 class Lexer {
+    std::string source;
 public:
-    Lexer();
+    // Reads the entire contents of a file into a string.
+    //
+    // This method utilizes the C++ Standard Library to read the contents of a file
+    // specified by its path. It is designed to read the file in binary mode to ensure
+    // that the data is read exactly as it is stored, without any transformations that
+    // might be applied in text mode.
+    //
+    // Parameters:
+    //  path - The path to the file to be read.
+    //
+    // Returns:
+    //  A string containing the entire contents of the file.
+    //
+    // Throws:
+    //  std::runtime_error - If the file could not be opened. The exception message
+    //  contains the path to aid in debugging the cause of the error.
+    //
+    // Example Usage:
+    //  std::string source = Lexer::readFile("path/to/file.sl");
+    //
+    static std::string readFile(const std::string &path);
+public:
+    Lexer(const std::string& sourcePath);
     ~Lexer();
 
 
